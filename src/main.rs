e5ccde472ca4fa61880d1038705458563f1a3aae
Noah Lundie <86733783+n-lundie@users.noreply.github.com>
2025-02-04 23:50:22 +0000

    add simple input file ingestion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs};
+
+fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return Err("must provide a path to input file".into());
+    }
+
+    let input = fs::read_to_string(&args[1]);
+
+    if let Err(_) = input {
+        return Err("invalid input path".into());
+    }
+
+    return Ok(());
 }
