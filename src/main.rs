531f11550f7252e4bc9a4d44b0996688dcad1e17
Noah Lundie <86733783+n-lundie@users.noreply.github.com>
2025-02-05 00:10:12 +0000

    create get_timestamp helper function, create positive unit test for parse csv

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, fs};
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 fn main() -> Result<(), String> {
     let input = ingest_file()?;
@@ -26,4 +26,34 @@ fn ingest_file() -> Result<String, String> {
 #[derive(PartialEq, Debug)]
 struct CsvRow(DateTime<Utc>, String, String, String);
 
-fn parse_csv(input: String) -> Result<Vec<CsvRow>, String> {}
+fn parse_csv(input: String) -> Result<Vec<CsvRow>, String> {
+    return Ok(vec![]);
+}
+
+#[cfg(test)]
+mod parse_csv {
+    use super::*;
+
+    #[test]
+    fn should_parse_valid_input() {
+        let input = String::from("11:35:23,scheduled task 032, START,37980\n11:35:56,scheduled task 032, END,37980\n11:36:11,scheduled task 796, START,57672");
+        let result = parse_csv(String::from(input));
+
+        let expected_value = CsvRow(
+            get_timestamp(11, 35, 23),
+            String::from("scheduled task 032"),
+            String::from("START"),
+            String::from("37980"),
+        );
+
+        match result {
+            Ok(e) => assert_eq!(e[0], expected_value),
+            Err(e) => panic!("Expected Ok, received: {}", e),
+        }
+    }
+}
+
+fn get_timestamp(hour: u32, minute: u32, second: u32) -> DateTime<Utc> {
+    // logs do not provide year, month or date so we provide defaults
+    return Utc.with_ymd_and_hms(0, 1, 1, hour, minute, second).unwrap();
+}
