5c49b71509ae5faf1f18691ba7f5ddac23b9b5be
Noah Lundie <86733783+n-lundie@users.noreply.github.com>
2025-02-05 00:34:01 +0000

    implement parse_csv, add documentation comments to CsvRow

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,52 @@ fn ingest_file() -> Result<String, String> {
 }
 
 #[derive(PartialEq, Debug)]
+/// Represents a row in a csv file
+///
+/// # Fields
+///
+/// * `0` the time the log was created
+/// * `1` a description of the process
+/// * `2` the status of the process ("START" or "END")
+/// * `3` the process id
 struct CsvRow(DateTime<Utc>, String, String, String);
 
 fn parse_csv(input: String) -> Result<Vec<CsvRow>, String> {
-    return Ok(vec![]);
+    let mut data = vec![];
+
+    for (i, e) in input.lines().enumerate() {
+        let input_columns: Vec<&str> = e.split(",").map(|e| e.trim()).collect();
+
+        if input_columns.len() > 4 {
+            return Err(format!("unexpected column, line {}", i));
+        }
+
+        // seperate timestamp into segments and try to parse to u32 otherwise return error
+        let time_segments: Vec<u32> = input_columns[0]
+            .split(":")
+            .map(|e| {
+                e.parse::<u32>()
+                    .map_err(|_| format!("invalid timestamp, line {}", i))
+            })
+            .collect::<Result<Vec<u32>, String>>()?;
+
+        let timestamp = get_timestamp(time_segments[0], time_segments[1], time_segments[2]);
+
+        let status = input_columns[2].to_string();
+
+        if status.ne("START") && status.ne("END") {
+            return Err(format!("invalid status, line {}", i));
+        }
+
+        data.push(CsvRow(
+            timestamp,
+            input_columns[1].to_string(),
+            status,
+            input_columns[3].to_string(),
+        ));
+    }
+
+    return Ok(data);
 }
 
 #[cfg(test)]
@@ -54,6 +96,6 @@ mod parse_csv {
 }
 
 fn get_timestamp(hour: u32, minute: u32, second: u32) -> DateTime<Utc> {
-    // logs do not provide year, month or date so we provide defaults
+    // logs do not provide year, month or day so we provide defaults
     return Utc.with_ymd_and_hms(0, 1, 1, hour, minute, second).unwrap();
 }
